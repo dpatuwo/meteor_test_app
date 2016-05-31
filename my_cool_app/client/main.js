79f7b90ab63997a52010e3c8b83f946281e9e492
Ralph Acevedo <rjaceved@gmail.com>
2016-05-30 21:36:15 -0400

    added notes to the code

--- my_cool_app/client/main.js
+++ my_cool_app/client/main.js
@@ -1,40 +1,46 @@
 Messages = new Mongo.Collection("msgs");
+// This code runs on both client (mini Mongo, Meteor's client-side cache; mini Mongo will keep a subset of the data that's on the server) and server (Production MongoDB database)
 
 if (Meteor.isServer) {
   // This code only runs on the server
+  // "Meteor.publish" gets data from the server to the client
   Meteor.publish("messages", function() {
     return Messages.find({}, {sort: {createdAt: -1}, limit: 5});
+    // 5 most recent messages to be sent to client
   });
   }
 
 
   if (Meteor.isClient) {
     // This code only runs on the client
+    // Subscribe to Meteor.publish (publication) to recieve messages; similar to a REST end point
     Meteor.subscribe("messages");
   }
 
+// This code sends data (messages) from the client to the server
   Meteor.methods({
     sendMessage: function (message) {
       if(! Meteor.userId()) {
         throw new Meteor.Error("not-authorized");
-        }
+        } // user authentication
+        //prevents users from sending messages when not logged in
 
         Messages.insert({
           text: message,
           createdAt: new Date(),
-          username: Meteor.user().username
+          username: Meteor.user().username // user authentication
         });
       }
   });
 
   if (Meteor.isClient) {
-    Template.body.helpers({
+    Template.body.helpers({ // publishes an array of messages, displayed in forward chronological order
       recentMessages: function () {
         return Messages.find({}, {sort: {createdAt: 1}});
       }
     });
 
-  Template.body.events({
+  Template.body.events({ // event handler
     "submit .new-message": function (event) {
       var text = event.target.text.value;
 
@@ -43,7 +49,7 @@ if (Meteor.isServer) {
       event.target.text.value = "";
       return false;
     }
-  });
+  }); // replicates to multiple clients at the same time, clients connected to the server; when we save to the db, it reactively changes our interface, and then when the data gets up to the db, it will push down any changes to any connected clients
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
